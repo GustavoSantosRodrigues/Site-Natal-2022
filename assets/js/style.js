366c6451eacce40c7e70b7c7d5672939f8dc1382
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2022-12-17 20:23:12 -0300

    section home feita

--- assets/js/style.js
+++ assets/js/style.js
@@ -26,3 +26,34 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/**CONTAGEM REGRESSIVA */
+const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
+const loadingContainer = document.querySelector('#loading')
+const countdownContainer = document.querySelector('#countdown')
+
+const eventDate = new Date('December 25 2022 00:00:00')/**inserir a data do final do evento */
+
+const updateCountdown = () =>{
+  const currenTime = new Date()
+  const difference = eventDate - currenTime
+  const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+  const hours = Math.floor(difference / 1000 / 60 / 60 ) % 24
+  const minutes = Math.floor(difference / 1000 / 60 ) % 60
+  const seconds = Math.floor(difference / 1000 ) % 60
+
+  secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+  minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+  hoursContainer.textContent = hours < 10 ? '0' + hours : hours
+  daysContainer.textContent = days < 10 ? '0' + days : days
+}
+
+setTimeout( () =>{
+  loadingContainer.remove()
+  countdownContainer.style.display = 'flex'
+}, 1000)
+
+setInterval(updateCountdown, 1000)
